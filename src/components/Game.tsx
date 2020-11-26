70a28aae8019b1c32de23e4a15362a5d190f4199
Jacob Penny <jacob.penny@gmail.com>
2020-11-26 16:08:12 -0400

    More than one type of bug

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import _ from 'lodash';
 import { useAnimationFrame } from '../hooks/useAnimationFrame';
 
 import './Game.scss';
@@ -17,9 +18,14 @@ interface Velocity {
   speed: number;
 }
 
+type ObjectType = 'flu' | 'covid';
+
+const objectTypes: ObjectType[] = ['flu', 'covid'];
+
 interface ObjectProps {
   position: Position;
   velocity: Velocity;
+  type: ObjectType;
 }
 
 interface WorldProps {
@@ -64,6 +70,7 @@ export function Game() {
           angle: Math.random() * Math.PI * 2,
           speed: Math.floor(Math.random() * 201) + 50,
         },
+        type: _.sample(objectTypes) as ObjectType,
       });
     }
 
@@ -90,7 +97,7 @@ export function Game() {
       // Be careful putting console.log calls in this callback as they will
       // put output on the console on every animation frame, up to 60 times per
       // second.
-      const updatedObjects = objects.map(({ position, velocity }) => {
+      const updatedObjects = objects.map(({ position, velocity, type }) => {
         // Get change in position scaled by the animation frame time. This uses
         // vector component formulas from high-school physics to get delta-x and
         // delta-y values:
@@ -110,6 +117,7 @@ export function Game() {
         return {
           position: newPosition,
           velocity,
+          type,
         };
       });
 
@@ -171,10 +179,19 @@ export function Game() {
 
   return (
     <div className="game" style={{ width, height }} ref={gameRef}>
-      {objects.map(({ position, velocity }, index) => (
+      {objects.map((object, index) => (
+        <GameObject key={index} {...object} />
+      ))}
+    </div>
+  );
+}
+
+function GameObject({ position, velocity, type }: ObjectProps) {
+  switch (type) {
+    case 'flu':
+      return (
         <div
-          className="game__object"
-          key={index}
+          className="flu"
           style={{
             color: '#fff',
             borderLeft: '20px solid rgba(100, 180, 220, 0.7)',
@@ -189,7 +206,27 @@ export function Game() {
             transform: `rotate(${velocity.angle}rad)`,
           }}
         />
-      ))}
-    </div>
-  );
+      );
+    case 'covid':
+      return (
+        <div
+          className="covid"
+          style={{
+            color: '#fcba03',
+            borderLeft: '20px solid rgba(252, 186, 3, 0.7)',
+            borderRight: '20px solid transparent',
+            borderTop: '7px solid transparent',
+            borderBottom: '7px solid transparent',
+            position: 'absolute',
+            top: position.y - 7,
+            left: position.x - 20,
+            width: 0,
+            height: 0,
+            transform: `rotate(${velocity.angle}rad)`,
+          }}
+        />
+      );
+    default:
+      return <div />;
+  }
 }
